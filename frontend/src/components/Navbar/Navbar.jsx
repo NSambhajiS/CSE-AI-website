01bc8f5b5e3f2a32837679ccc79968314647198a
NSambhajiS <neha.22210877@viit.ac.in>
2025-03-07 18:03:38 +0530

    Navbar styles added

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,33 +1,43 @@
-// filepath: /krishna-project/frontend/krishna-frontend/src/components/Navbar/Navbar.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
+import logo from '../../assets/logo.png';
+import menu_icon from '../../assets/menu-icon.png';
 
 function Navbar() {
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setMobileMenu(!mobileMenu);
+  };
+
   return (
-    <nav>
-      <ul>
+    <nav className="navbar">
+      <img src={logo} alt="Logo" className="logo" />
+      <ul className={`nav-links ${mobileMenu ? 'show-mobile-menu' : ''}`}>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" onClick={() => setMobileMenu(false)}>Home</Link>
         </li>
         <li>
-          <Link to="/research">R&D</Link>
+          <Link to="/research" onClick={() => setMobileMenu(false)}>R&D</Link>
         </li>
         <li>
-          <Link to="/faculties">Faculties</Link>
+          <Link to="/faculties" onClick={() => setMobileMenu(false)}>Faculties</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about" onClick={() => setMobileMenu(false)}>About</Link>
         </li>
         <li>
-          <Link to="/hod">HoD</Link>
+          <Link to="/hod" onClick={() => setMobileMenu(false)}>HoD</Link>
         </li>
         <li>
-          <Link to="/admin">Admin Login</Link>
+          <Link to="/admin" onClick={() => setMobileMenu(false)} className="btn">Admin Login</Link>
         </li>
         <li>
-          <Link to="/faculty">Faculty Login</Link>
+          <Link to="/faculty" onClick={() => setMobileMenu(false)} className="btn">Faculty Login</Link>
         </li>
       </ul>
+      <img src={menu_icon} alt="Menu" className="menu-icon" onClick={toggleMenu} />
     </nav>
   );
 }
